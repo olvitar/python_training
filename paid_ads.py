e6ed1a65ccba16010a33e890adec4211d018e284
Alex Taran <olvitar84@gmail.com>
2018-09-21 10:49:33 +0300

    Add scroll, resulted screenshot, correct quit
    Added scrolling to the element,
    screenshot of the resulted page,
    correct quit of the driver

--- paid_ads.py
+++ paid_ads.py
@@ -16,24 +16,30 @@ def login():
     driver.save_screenshot('screenshot1.png')  # make a screenshot of the paige
 
 def paid_adv():
-    try:
-        driver.find_element_by_css_selector("button.allpageinside_allthepage_inside_pagein_adstoclickonce_actions_view").click()
-        driver.find_element_by_xpath("//div[@id='allpageinside_allthepage_inside_pagein_viewadsporter']/a/button").click()
-        element = WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.ID, 'body_headerviewads_countdown_confirm_button')))
-        element.click()
-    except:
-        print('You watched all bonus adverts for today')
+    driver.find_element_by_css_selector(
+        "button.allpageinside_allthepage_inside_pagein_adstoclickonce_actions_view").click()
+    driver.find_element_by_xpath("//div[@id='allpageinside_allthepage_inside_pagein_viewadsporter']/a/button").click()
+    element = WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((By.ID, 'body_headerviewads_countdown_confirm_button')))
+    element.click()
 
 def logout():
     driver.find_element_by_partial_link_text('logout').click()
 
+#start
 login()
-
-i=0
-while i<21:
-    paid_adv()
-    i += 1
-    print("You watched ", i , " advertisement")
-print("Everything is Ok")
-logout()
-driver.quit()
+try:
+    i: int = 0
+    while i < 21:
+        paid_adv()
+        i += 1
+        print("You watched ", i, " advertisement")
+    print("Everything is Ok")
+    logout()
+except:
+    print('You watched all bonus adverts for today or some issue is occurred. Check the screenshot.')
+    footer = driver.find_element_by_class_name("socials") #find element Footer
+    footer.location_once_scrolled_into_view  #scroll page to the element Footer
+    driver.save_screenshot('screenshot1.png')  # make a screenshot of the paige
+finally:
+    driver.quit()
